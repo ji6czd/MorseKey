9448576758e88a15357ad405f430dbc37b261d3e
KIRIAKE Masanori <seiken@ARGV.org>
2018-07-16 11:00:04 +0900

    Fix

--- MorsePi.cpp
+++ MorsePi.cpp
@@ -66,5 +66,6 @@ uint16_t MorsePi::pulseLength(bool defStat)
 	}
 	clock_gettime(CLOCK_MONOTONIC, &end);
 	uint16_t len = (end.tv_nsec-start.tv_nsec)*1000;
-	return len+(end.tv_nsec-start.tv_nsec)*1000;
+	//return len+(end.tv_nsec-start.tv_nsec)*1000;
+	return len;
 }
